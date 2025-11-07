b7a9707b4cbc91af1dae41435ee02a558752c2b4
temurqobilov1 <qobilovtemuruz@gmail.com>
2025-11-07 14:39:15 +0500

    done

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import "@ant-design/v5-patch-for-react-19";
 import ProviderConf from "./routes/provider";
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ProviderConf>
--- src/routes/provider/index.tsx
+++ src/routes/provider/index.tsx
@@ -1,7 +1,6 @@
 import { type ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
-
 const ProviderConf = ({ children }: { children: ReactNode }) => {
   const queryClient = new QueryClient();
   return (
